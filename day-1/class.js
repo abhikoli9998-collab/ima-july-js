1dbf42524330c69a3c5f0271ee89f235d0f4b5b1
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-07-23 10:13:32 +0530

    update

---
+++ day-1/class.js
@@ -0,0 +1,76 @@
+// let userName = "Abhishek";
+// console.log("Hi")
+// console.log(24)
+// console.log(true)
+// console.log(userName)
+// console.log(2 + 2)
+
+// let userAge = 25;
+// console.log(userAge, "- user age", 24, true, false, "Hii", 'welcome')
+
+// var age = 24; // declare and assign 
+// console.log(age, "- age with declare and assign")
+// age = 25 //; re-assign 
+// console.log(age, "- age with re-assign")
+// var age = 26;
+// console.log(age, "- age with re-declared")
+
+// let age = 25;
+// console.log(age, "- age with declare and assign")
+// age = 26;
+// console.log(age, "- age with re-assignF")
+// let age = 27;
+// console.log(age, "- age with re-declare")
+
+// const age = 25;
+// console.log(age, "- age with declare and assign")
+// age = 26;
+// console.log(age, "- age with re-assignF")
+// const age = 27;
+// console.log(age, "- age with re-declare")
+
+
+// var age = 25;
+// console.log(age)
+// {
+//     console.log(age)
+// }
+
+
+// {
+//     var age = 25;
+//     console.log(age)
+// }
+// console.log(age)
+
+// {
+//     let age = 25;
+//     {
+//         console.log(age, "- inner log")
+//     }
+// }
+// console.log(age, "- outer log")
+
+// {
+//     {
+//         {
+//             const age = 25;
+//         }
+//         console.log(age)
+//     }
+// }
+
+
+// var number1 = 1;
+// {
+//     let number2 = 2;
+//     {
+//         let number3 = 3.1;
+//         {
+//             const number3 = 3;
+//         }
+//         console.log(number3) // 3.1
+//     }
+//     console.log(number2) // error 2
+// }
+// console.log(number1) // 1
