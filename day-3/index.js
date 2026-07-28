9bf0ba80b38a8fb0dac1cc0fb46814087278367f
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-07-28 21:23:59 +0530

    update

--- day-3/index.js
+++ day-3/index.js
@@ -1,2 +1,96 @@
-console.log(2 == 2);
-console.log(2 >= 4);
+function kahipn (){
+    console.log("Abhishek")
+}
+kahipn()
+
+function addNumbers (num1, num2){
+    console.log(22)
+}
+addNumbers()
+function studentName (){
+    console.log ("hello")
+}
+studentName();
+studentName()
+studentName()
+studentName()
+studentName()
+studentName();
+studentName()
+studentName()
+studentName()
+studentName()
+
+function studentName (){
+    console.log ("hello")
+}
+studentName();
+studentName()
+studentName()
+studentName()
+studentName()
+studentName();
+studentName()
+studentName()
+studentName()
+studentName()
+
+function sayHi (){
+    console.log("Hi");
+}
+console.log("start");
+
+console.log("End");
+sayHi();
+ 
+
+function greet (name){
+    console.log(name);
+
+}
+greet("kaveri");
+greet("Abhishek");
+greet("Renuka");
+
+function greet (name){
+    console.log ( "Hello" +name)
+}
+greet ( "kaveri")
+
+function introduction (name, age){
+    console.log(name)
+    console.log(age)
+}
+introduction("kaveri", 25);
+
+function add (a,b){
+    console.log(a+b)
+}
+add (210,300)
+
+function add (a,b){
+    return a+b;
+}
+let ans = add (10, 20);
+console.log(ans);
+
+function greet (){
+    console.log("Hello")
+}
+greet()
+
+// Arrow Function 
+// () paranthysis
+// {} curly braces
+// [] square braces
+//  => Arrow
+
+const hello = () => {
+    console.log("HEllo")
+}
+hello();
+
+// const add = (a,b) => {
+//     console.log(a*b);
+// }
+// add(10,10);
