9563abff3e6938d702a558776401dcd4d5ea3add
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-07-31 12:02:42 +0530

    update

---
+++ day-4/index.js
@@ -0,0 +1,27 @@
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+console.log(fruits)
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+console.log(fruits[0])
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+console.log(fruits[4])
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+fruits.push("kiwi")
+console.log(fruits)
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+fruits.pop()
+console.log(fruits)
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+fruits.unshift("apple")
+console.log(fruits)
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+fruits.shift()
+console.log(fruits)
+
+let fruits = ["mango","apple","banana","grapes","strawberry"]
+console.log(fruits.length)
