2823e4f71007959a77ea4832fb7bd323eb7c2ce3
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-08-03 19:50:11 +0530

    update

---
+++ day-5/index.js
@@ -0,0 +1,68 @@
+// const student = {
+//   name: "Abhishek",
+//   age: 21,
+//   course: "BCA",
+//   rollNo: 101,
+//   city: "Solapur",
+
+//   showDetails: function () {
+//     console.log(this.name, this.age, this.course, this.rollNo, this.city);
+//   }
+// };
+
+// console.log("Student Details:");
+// student.showDetails();
+
+
+
+
+// const car = {
+//   brand: "Toyota",
+
+//   start: function () {
+//     console.log(this.brand + " Car Started");
+//   },
+
+//   stop: function () {
+//     console.log(this.brand + " Car Stopped");
+//   }
+// };
+
+// car.start();
+// car.stop();
+
+
+// const bankAccount = {
+//   accountHolder: "Abhishek",
+//   balance: 5000,
+//   deposit: function (amount) {
+//     this.balance += amount;
+//     console.log(this.balance);
+//   },
+//   withdraw: function (amount) {
+//     this.balance -= amount;
+//     console.log(this.balance);
+//   }
+// };
+// bankAccount.deposit(2000);
+// bankAccount.withdraw(1000);
+
+
+// const shoppingCart = {
+//   items : ["pants", "shirt", "shoes"],
+//   itemsprice : [1000, 500, 2000],
+//     calculateTotal: function () {
+//         total = 1000 + 500 + 2000;
+//         console.log("Total Price: " + total);
+//     }   
+// }
+// shoppingCart.calculateTotal();
+// const mobile = {
+//   brand: "Samsung",
+//   price: 25000,
+//   color: "Black",
+//   displayInfo: function () {
+//     console.log(this.brand, this.price, this.color);
+// }
+// }
+// mobile.displayInfo();
