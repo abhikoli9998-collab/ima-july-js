5267efb5f68ae894b1c0035f7494d5ed6ee641bc
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-09-02 21:51:17 +0530

    update

---
+++ day-7/index1.js
@@ -0,0 +1,12 @@
+// let number = 7;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(number + " x " + i + " = " + (number * i));
+// }
+
+// for (let n = 1; n <= 50; n++) {
+//     if (n % 2 === 0)
+//         console.log(n + " → Even");
+//     else
+//         console.log(n + " → Odd");
+// }
