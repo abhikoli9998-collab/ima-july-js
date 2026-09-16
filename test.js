10d69b30035c890bbe5b886ddc7b091ab0d7ebcc
abhikoli9998-collab <abhikoli4897@gmail.com>
2026-09-16 22:00:04 +0530

    update

---
+++ test.js
@@ -0,0 +1,40 @@
+// let a = 10;
+// let b = "10";
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a == b);
+// console.log(a === b);
+
+// let marks = 95;
+// if (marks >= 90){
+//     console.log("A Grade")
+// }
+// else if (marks >= 75){
+//     console.log("B Grade")
+// }
+// else if (marks >=50){
+//     console.log("C Grade")
+// }
+// else {
+//     console.log("Fail")
+// }
+
+// let numbers = [10, 25, 30, 45, 50, 65, 70];
+// for(let number of numbers){
+//     console.log(number);
+// }
+// for(let number of numbers){
+//     if(number>40)
+//     console.log(number);
+// }
+
+// let students = [ 
+//     { name: "Amit", age: 22, marks: 85 }, 
+//     { name: "Priya", age: 21, marks: 45 }, 
+//     { name: "Rahul", age: 23, marks: 72 }, 
+//     { name: "Sneha", age: 20, marks: 90 } ];
+// let result = students.filter(student =>student.marks > 70)
+// console.log(result)
+// let names = students.map(student => student.name);
+// console.log(names);
+
